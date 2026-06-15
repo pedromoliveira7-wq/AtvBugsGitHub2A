56256f7d4c17f49c984e97e1e5e1e4aabdcb2195
pedromoliveira7-wq <pedro.m.oliveira7@edu.senai.br>
2026-06-15 08:44:16 -0300

    quarto erro

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ def eh_palindromo(palavra):
     """Retorna True se a palavra for um palíndromo."""
     palavra = palavra.lower()
     invertida = palavra[::-1]
-    if palavra != invertida:  # BUG: deveria ser ==
+    if palavra == invertida:  # BUG: deveria ser ==
         return True
     else:
         return False
